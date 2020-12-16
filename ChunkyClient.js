a91c303ef4bddcf8a31ae4d05e27f298fce4f973
Rosario-Francesco Polito <rosario.polito@gmx.de>
2020-12-16 22:20:24 +0100

    added getPartialObject & putobject method

--- ChunkyClient.js
+++ ChunkyClient.js
@@ -1,12 +1,36 @@
 import axios from 'axios';
 
-let ChunkyClient = (endpoint, accessKey, secretKey) => ({
+let ChunkyClient = (endPoint, accessKey, secretKey) => ({
 
     async makeBucket(bucketName) {
         try {
-            await axios.post(endpoint + "/bucket/" + bucketName)
+            await axios.post(`${endPoint}/bucket/${bucketName}`)
         } catch (err) {
             console.error(err);
+        }
+    },
+
+    async fPutObject(bucketName, file) {
+        try {
+            const chunkyURL = `${endPoint}/file/upload/${bucketName}`;
+
+            let fileFormData = new FormData();
+            fileFormData.append("uploadedFile", file);
+
+            const headers = { 'filename': file.name }
+
+            await axios.post(chunkyURL, fileFormData, headers);
+
+        } catch (error) {
+            console.error(error);
+        }
+    },
+
+    async getPartialObject(bucketName, fileName) {
+        try {
+            return axios.get(`${endPoint}/file/${bucketName}/${fileName}`)
+         } catch (err) { 
+            console.log(err); 
         }
     },
 
